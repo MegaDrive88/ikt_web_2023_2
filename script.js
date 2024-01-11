abd78215d3a078df6888b989467504663cc52278
MegaDrive88 <podlipnik.adam@students.jedlik.eu>
2024-01-11 14:37:10 +0100

    small changes, watch out for awaits later!!!!

--- script.js
+++ script.js
@@ -19,24 +19,76 @@ class Biker{
         div.style.top = this.top + 'px'
         div.style.left = this.left + 'px'
         GRID.appendChild(div)
-        
     }
     turn(){
-
+        
     }
-    turbo(){
-
+    async move(direction){
+        while (true){
+            this.leaveMark(direction)
+            await Game.delay(25)
+        }
     }
-    jump(){
-
+    async turbo(direction){
+        for(let i = 0; i < 20; i++){
+            this.leaveMark(direction)
+            await Game.delay(10)
+        }
+    }
+    async jump(direction){
+        for(let i = 0; i < 10; i++){
+            this.theBikeItself.style.top = this.top + 5 + 'px'
+            this.top = parseInt(this.theBikeItself.style.top.replace('px', ''))
+            this.left = parseInt(this.theBikeItself.style.left.replace('px', ''))
+            await Game.delay(10)
+        }//color detectiont majd itt ki kéne kapcsolni        
     }
     death(){
 
     }
 }
 class BlueBiker extends Biker{
+    // turn = function(){
+        
+    // } ez az override
+    async Start(){
+        await player1.jump(1)
+        player1.turbo(1)
+    }
+}
+class OrangeBiker extends Biker{
+    async Start(){
+        player2.turbo(1)
+    }
+}
+class Game{
+    static fillGrid(){
+        for(let i = 0; i < 121; i++){
+            let tile = document.createElement("div")
+            tile.className = "gridTile"
+            GRID.appendChild(tile)
+        }
+    }
+    static delay(milliseconds) {
+        return new Promise(resolve => {
+            setTimeout(resolve, milliseconds)
+        });
+    }
+    static async Start(){
+        player1.Start()
+        player2.Start() 
+    }
 
 }
+const GRID = document.querySelector("#grid")
+Game.fillGrid()
+const blueBike = document.querySelector('#blue')
+const oranBike = document.querySelector('#orange')
+const player1 = new BlueBiker("blue", blueBike)
+const player2 = new OrangeBiker("oran", oranBike)
+
+
+Game.Start()
 // class DirectionNums{
 //     show(keycode) {
 //         switch(keycode){
@@ -59,36 +111,11 @@ class BlueBiker extends Biker{
 //         }
 //     }
 // }
-const GRID = document.querySelector("#grid")
-function fillGrid(){
-    for(let i = 0; i < 121; i++){
-        let tile = document.createElement("div")
-        tile.className = "gridTile"
-        GRID.appendChild(tile)
-    }
-}
-fillGrid()
 
-const blueBike = document.querySelector('#blue')
-const oranBike = document.querySelector('#orange')
-
-let player1 = new Biker("blue", blueBike)
-
-async function anyád(){
-    for(let i = 0; i < 50; i++){
-        player1.leaveMark(10)
-        await delay(25)
-    }
-
-}
-function delay(milliseconds) {
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds)
-    });
-}
-anyád()
-
-// const extractPixelColor = (cols: number, x: number, y: number) => {
+//cols: Width of the image representing total number of columns
+//x: Row position of this pixel
+//y: Column position of this pixel
+// const extractPixelColor = (cols, x, y) => {
 //     //To get the exact pixel, the logic is to multiply total columns in this image with the row position of this pixel and then add the column position of this pixed
 //     let pixel = cols * x + y;
 //     //To get the array position in the entire image data array, simply multiply your pixel position by 4 (since each pixel will have its own r,g,b,a in that order)
@@ -101,3 +128,4 @@ anyád()
 //       alpha: data[position + 3],
 //     };
 //   };
+// getImageData(x, y, 1, 1).data
